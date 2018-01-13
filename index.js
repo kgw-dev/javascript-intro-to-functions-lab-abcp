97b35fa516777c9dbad17133f3d0747dd526f467
kent winter <kgw700@gmail.com>
2018-01-13 17:47:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function shout(string) {
-  return string.toUpperCase()
+function logShout(string) {
+  console.log (string.toUpperCase());
 }
 
 function shout(string) {
